924ac89a3beffdc7a1436b86246ddc7c4e1bfb1b
134 <38720563+1-3-4@users.noreply.github.com>
2018-10-29 11:26:28 +0100

    Added gulp to project.
    Added to do file for project plan.

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+
+// Path to localhost on AspIT computer:
+var localhost = 'C:/apps/xampp/htdocs/';
+// Path to localhost on private computer:
+// var localhost = 'C:/apps/wamp64/www/';
+
+var paths = {
+	src: 'src/**/*',
+	srcHTML: 'src/**/*.html',
+	srcSCSS: 'src/bem/**/*.scss',
+	srcJS: 'src/**/*.js',
+
+	tmp: localhost + 'tmp',
+	tmpHTML: localhost + 'tmp/**/*.html',
+	tmpCSS: localhost + 'tmp/',
+	tmpJS: localhost + 'tmp/'
+};
+
+gulp.task('default', ['watch']);
+
+gulp.task('html', function () {
+	return gulp.src(paths.srcHTML)
+		.pipe(gulp.dest(paths.tmp))
+		.pipe(livereload());
+});
+
+gulp.task('css', function () {
+	return gulp.src(paths.srcSCSS)
+		.pipe(sass())
+		.pipe(gulp.dest(paths.tmpCSS))
+		.pipe(livereload());
+});
+
+gulp.task('js', function () {
+	return gulp.src(paths.srcJS)
+		.pipe(gulp.dest(paths.tmpJS))
+		.pipe(livereload());
+});
+
+gulp.task('watch', ['html', 'css', 'js'], function () {
+	livereload.listen();
+	gulp.watch(paths.srcHTML, ['html']);
+	gulp.watch(paths.srcSCSS, ['css']);
+	gulp.watch(paths.srcJS, ['js']);
+});
