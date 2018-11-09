c0e7f7caa93c82fd2f3b062ea79fe38b82e13c0f
134 <38720563+1-3-4@users.noreply.github.com>
2018-11-09 12:07:06 +0100

    Removed babel from gulpfile because of trouble with gulp-inject.
    gulp-inject can't find the JS file to inject.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
 var sass = require('gulp-sass');
-var babel = require('gulp-babel');
+// var babel = require('gulp-babel');
 var purgecss = require('gulp-purgecss');
 var concat = require('gulp-concat');
 var livereload = require('gulp-livereload');
@@ -15,55 +15,52 @@ var paths = {
 	src: 'src/**/*',
 	srcHTML: 'src/**/*.html',
 	srcSCSS: 'src/**/*.scss',
-	// srcSCSS: 'src/css/*.css',
 	srcJS: 'src/**/*.js',
 
 	tmp: localhost + 'tmp',
 	tmpHTML: localhost + 'tmp/**/*.html',
 	tmpCSS: localhost + 'tmp/**/*.css',
-	tmpJS: localhost + 'tmp/**/*js'
+	tmpJS: localhost + 'tmp/**/*.js'
 };
 
 gulp.task('default', ['watch']);
 
 gulp.task('html', function () {
-	return gulp.src(paths.srcHTML)
-		.pipe(gulp.dest(paths.tmp))
-		.pipe(livereload());
+    return gulp.src( paths.srcHTML )
+        .pipe( gulp.dest( paths.tmp ) );
 });
 
 gulp.task('css', function () {
-	return gulp
-		.src(paths.srcSCSS)
-		.pipe(sass())
-		.pipe( purgecss( { content: [ paths.srcHTML ] } ) )
-		.pipe(gulp.dest(paths.tmpCSS))
-		.pipe(livereload());
+    return gulp
+        .src( paths.srcSCSS )
+        .pipe( sass() )
+        .pipe( purgecss( { content: [ paths.srcHTML ] } ) )
+        .pipe( gulp.dest( paths.tmp ) );
 });
 
 gulp.task('js', function () {
-	// return gulp.src(paths.srcJS)
-    return gulp.src( ['./src/js/data.js', './src/js/ui.js', './src/js/ctrl.js'] )
-        .pipe(concat('script.js'))
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-		.pipe(gulp.dest(paths.tmpJS))
-		.pipe(livereload());
+    return gulp
+        // .src( paths.srcJS )
+        .src( ['./src/js/data.js', './src/js/ui.js', './src/js/ctrl.js'] )
+        // .pipe( babel( { presets: ['@babel/env'] } ) )
+        .pipe( concat( 'script.js' ) )
+        .pipe( gulp.dest( paths.tmp ) );
 });
 
-gulp.task('inject', [ 'html' ], function () {
+gulp.task('copy', ['html', 'css', 'js']);
+
+gulp.task('inject', ['copy'], function () {
     var css = gulp.src(paths.tmpCSS, {read:false});
     var js = gulp.src(paths.tmpJS, {read:false});
-    return gulp.src(paths.tmpHTML)
-        .pipe(inject( css, { relative:true, removeTags: true } ))
+    return gulp
+        .src(paths.tmpHTML)
+        .pipe(inject( css, { relative:true, removeTags:true } ))
         .pipe(inject( js, { relative:true, removeTags:true } ))
-        .pipe(gulp.dest(paths.tmp));
+        .pipe( gulp.dest( paths.tmp ) )
+        .pipe( livereload() );
 });
 
-gulp.task('watch', ['html', 'css', 'js'], function () {
-	livereload.listen();
-	gulp.watch(paths.srcHTML, ['html', 'css', 'inject']);
-	gulp.watch(paths.srcSCSS, ['css']);
-	gulp.watch(paths.srcJS, ['js']);
+gulp.task('watch', ['inject'], function () {
+    livereload.listen();
+    gulp.watch( paths.src, ['inject'] );
 });
